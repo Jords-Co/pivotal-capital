107f9c73872f57bb2368e78cc5441855d79337d3
Digerati <127671144+CabalOfDigerati@users.noreply.github.com>
2024-11-22 20:18:38 +0000

    Update headingCircleHighlight.ts

--- src/pivotalCapital/headingCircleHighlight.ts
+++ src/pivotalCapital/headingCircleHighlight.ts
@@ -6,16 +6,11 @@
 export const headingCircleHighlight = () => {
     const circleWrappers = document.querySelectorAll('[dd-circle-highlight="wrapper"]'),
         circleElements = document.querySelectorAll('[dd-circle-highlight="element"]');
-    console.log(circleWrappers);
-    console.log(circleElements);
     if (!circleWrappers || !circleElements) {
         return;
     }
-    console.log('present!');
     circleWrappers.forEach((circleWrapper, index) => {
         let circleElement = circleElements[index];
-        console.log(circleWrapper);
-        console.log(circleElement);
         circleWrapper.appendChild(circleElement.cloneNode(true));
     });
 };
