4ab7a74a7b026d18cc53b1bb549cecf6177573b9
Digerati <cabal@digerati.design>
2024-11-26 18:15:08 +0000

    Safari clip mask bug fix?

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { skipToMainContent } from "$digerati/skipToMainContent";
 import { currentYear } from "$digerati/currentYear";
 import { headingCircleHighlight } from "./pivotalCapital/headingCircleHighlight";
+import { fixSafariClipPathBug } from "./pivotalCapital/fixSafariClipPathBug";
 
 window.Webflow || [];
 window.Webflow.push(() => {
   skipToMainContent();
   currentYear();
   headingCircleHighlight();
+  fixSafariClipPathBug();
 });
---
+++ src/pivotalCapital/fixSafariClipPathBug.ts
@@ -0,0 +1,16 @@
+/**
+ * Fix Safari Clip Path Bug.
+ * 
+ * @author <cabal@digerati.design>
+ */
+export const fixSafariClipPathBug = () => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isSafari = userAgent.indexOf('safari/') > -1 && userAgent.indexOf('chrome/') === 0;
+    const clipPathElements = document.querySelectorAll('[dd-clip-path="true"]');
+    if (!isSafari || !clipPathElements) {
+        return;
+    }
+    clipPathElements.forEach((clipPathElement) => {
+        clipPathElement.style.backdropFilter = 'none';
+    });
+}
